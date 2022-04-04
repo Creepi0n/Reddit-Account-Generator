69933400bf75c945d31f137c1201edae023059aa
Creepi0n <55346091+Creepi0n@users.noreply.github.com>
2022-04-04 23:53:22 +0200

    Update namesforreddit.py

--- namesforreddit.py
+++ namesforreddit.py
@@ -12,7 +12,19 @@ import re
 import string
 import secrets
 import os
-driver = webdriver.Chrome(ChromeDriverManager().install()) # USES CHROMEDRIVERMANAGER TO AUTO UPDATE CHROMEDRIVER
+from selenium.webdriver.chrome.options import Options
+tor_proxy = "127.0.0.1:9150"
+
+chrome_options = Options()
+
+'''chrome_options.add_argument("--test-type")'''
+chrome_options.add_argument('--ignore-certificate-errors')
+'''chrome_options.add_argument('--disable-extensions')'''
+chrome_options.add_argument('disable-infobars')
+'''chrome_options.add_argument("--incognito")'''
+chrome_options.add_argument('--user-data=C:\\Users\\user\\AppData\\Local\\Google\\Chrome\\User Data\\Default')
+chrome_options.add_argument('--proxy-server=socks5://%s' % tor_proxy)
+driver = webdriver.Chrome(executable_path=ChromeDriverManager().install(), options=chrome_options)
 
 # GENERATE PASSWORD
 alphabet = string.ascii_letters + string.digits
